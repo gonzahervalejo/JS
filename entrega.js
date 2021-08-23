ac73a9bef6b661602d9d83e7f577cb49deedb07a
gonzahervalejo <gonzalohervalejo@hotmail.com>
2021-08-23 19:36:34 -0300

    segunda entrega

--- entrega.js
+++ entrega.js
@@ -1,7 +1,29 @@
 
 // clase 1
 
-const edad=parseInt(prompt("¿Eres mayor de edad? Ingrese su edad"));
-alert("Mi edad es " + edad);
+// const edad=parseInt(prompt("¿Eres mayor de edad? Ingrese su edad"));
+// alert("Mi edad es " + edad);
+
+
+
+// clase 2
+
+const nombreUsuario=prompt("Ingrese su Usuario");
+
+const clave=prompt("Ingrese su clave personal");
+
+if ((nombreUsuario == "gonzah")||(nombreUsuario == "Gonzah") && (clave == "gonzahervalejo")) {
+    
+    alert("Bienvenido GonzaH");
+}
+
+else {
+
+    alert ("Intruso");
+}
+
+
+
+
 
 
