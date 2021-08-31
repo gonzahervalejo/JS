114dbaaffc6b3fba2ef3036657f85677eb3d60e8
gonzahervalejo <gonzalohervalejo@hotmail.com>
2021-08-30 23:55:03 -0300

    entrega 4

--- entrega.js
+++ entrega.js
@@ -28,16 +28,43 @@
 
 // turnos
 
-for (let i = 1; i <=10; i++) {
-let ingresarNombre = (prompt("Igresar nombre"));
-console.log( "Tienes " +i+ " personas adelante " +ingresarNombre);
+// for (let i = 1; i <=10; i++) {
+// let ingresarNombre = (prompt("Igresar nombre"));
+// console.log( "Tienes " +i+ " personas adelante " +ingresarNombre);
+// }
+
+// let i = false;
+// do{
+//     console.log("No hay mas turnos");
+
+// } 
+
+
+
+// clase 4
+
+let bienvenido = (prompt("Bienvenido, Ingrese su nombre"));
+
+
+
+function turno(){
+
+    for (let i=1; i <=10; i++){
+        console.log(bienvenido  +i+ " personas adelante ");
+    }
+
+    if (bienvenido ==""){
+        alert ("usted no ingreso su nombre")
+    }
+    
+
+   
+
+  
 }
 
-let i = false;
-do{
-    console.log("No hay mas turnos");
 
-} while(i);
+
 
 
 
